743f4dd744ac9c88fd0c3ae144914caa25a617bc
VenuModi <venu.modi@iths.se>
2023-05-20 20:23:35 +0200

    Create Jwt Auth filter class and Jwt service class

---
+++ src/main/java/com/example/authms/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.example.authms.config;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String Token) {
+        return null;
+    }
+}
