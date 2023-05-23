9c220b4f4f7fbf0bc4122f15754fbd6041f750b1
VenuModi <venu.modi@iths.se>
2023-05-23 21:59:57 +0200

    Add docker file and remove test

--- src/test/java/com/example/authms/AuthMsApplicationTests.java
+++ src/test/java/com/example/authms/AuthMsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.authms;
+/*package com.example.authms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class AuthMsApplicationTests {
     }
 
 }
+*/
