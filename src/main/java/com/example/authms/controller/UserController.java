16476be7bf44dfb0e5abc321a0deccf5075f5f0b
VenuModi <venu.modi@iths.se>
2023-05-26 13:28:47 +0200

    Modify files

--- src/main/java/com/example/authms/controller/UserController.java
+++ src/main/java/com/example/authms/controller/UserController.java
@@ -7,27 +7,31 @@ import com.example.authms.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
 
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("")
 public class UserController {
 
     private final UserService userService;
     private final JwtGeneratorInterface jwtGenerator;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserService userService, JwtGeneratorInterface jwtGenerator){
+    public UserController(UserService userService, JwtGeneratorInterface jwtGenerator, PasswordEncoder passwordEncoder){
         this.userService=userService;
         this.jwtGenerator =jwtGenerator;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> postUser(@RequestBody User user){
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userService.saveUser(user);
             return new ResponseEntity<>(user, HttpStatus.CREATED);
         }catch (Exception e){
@@ -35,14 +39,14 @@ public class UserController {
         }
     }
 
-    @PostMapping("/login")
+    @PostMapping("/auth")
     public ResponseEntity<?> loginUser(@RequestBody User user){
         try {
             if (user.getUsername() == null || user.getPassword() == null){
                 throw new UserNotFoundException("Username or password is Empty");
             }
             User userData = userService.getUserByNameAndPassword(user.getUsername(), user.getPassword());
-            if (userData == null){
+            if (userData == null || !passwordEncoder.matches(user.getPassword(), userData.getPassword() )){
                 throw new UserNotFoundException("Username or password is invalid");
             }
             return new ResponseEntity<>(jwtGenerator.generateToken(user), HttpStatus.OK);
